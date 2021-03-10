0b0e819e4ee7917ef7d34c6b9b028e02e803a568
Michell Cronberg <devcr@DESKTOP-20RUL9Q>
2021-03-10 14:58:29 +0100

    Første commit

---
+++ MatCro.Partier.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MatCro.Partier.Kerne;
+
+namespace MatCro.Partier.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            //int r = p.LægSammen(15, 2);
+            //Console.WriteLine(r);
+            
+            //Console.WriteLine(p.TrækFra(309, 23));
+
+            //Console.WriteLine(p.TrækFraSpecial(10, 5));
+            
+            //p.SkrivUd("test");                 
+
+            
+            PartiSpil p = new PartiSpil();
+            string[] partier = p.HentPartier();
+            string[] partiledere = p.HentPartiledere();
+
+
+            for (int i = 0; i < partier.Length; i++)
+            {
+                Console.WriteLine(partier[i] + " " + partiledere[i]);
+            }
+
+
+
+        }
+
+
+
+    }
+}
---
+++ MatCro.Partier.Kerne/PartiSpil.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatCro.Partier.Kerne
+{
+    public class PartiSpil
+    {
+
+        public string[] HentPartier()
+        {
+            string[] partier = {
+                "Radikale venstre",
+                "Socialdemokratiet",
+                "Venstre",
+                "Enhedslisten",
+                "Dansk Folkeparti",
+                "Nye Borgerlige",
+                "Det Konservative Folkeparti",
+                "Socialistisk Folkeparti",
+                "Liberal Alliance",
+                "Alternativet"
+            };
+            return partier;
+        }
+
+        public string[] HentPartiledere()
+        {
+            string[] partiledere =
+            {
+                "Sofie Carsten Nielsen",
+                "Mette Frederiksen",
+                "Jakob Ellemann-Jensen",
+                "Mai Villadsen",
+                "Kristian Thulesen Dahl",
+                "Pernille Vermund",
+                "Søren Pape Poulsen",
+                "Pia Olsen Dyhr",
+                "Alex Vanopslagh",
+                "Bente Holm Villadsen"
+            };
+            return partiledere;
+        }
+
+        //public int LægSammen(int tal1, int tal2)
+        //{
+        //    int res = tal1 + tal2;
+        //    return res;        
+        //}
+
+        //public int TrækFra(int tal1, int tal2)
+        //{
+        //    int res = tal1 - tal2;
+        //    return res;
+        //}
+
+        //public int TrækFraSpecial(int tal1, int tal2)
+        //{
+
+        //    int sek = DateTime.Now.Second;
+        //    if (sek % 2 == 0)
+        //    {
+        //        return tal1 - tal2;
+
+        //    }
+        //    else
+        //    {
+        //        return 0;
+        //    }
+            
+
+
+        //}
+
+        //public void SkrivUd (string tekst)
+        //{
+        //    Console.WriteLine(" - " + tekst);
+        //} 
+
+
+
+
+
+    }
+}
